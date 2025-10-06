1bb12c6cde190fc1b1793fcfce753002666761fd
poojashelke8 <poojabshelke0807@gmail.com>
2025-10-06 19:04:28 +0530

    add register controller and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,71 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-
+import {ApiError} from "../utils/ApiError.js"
+import { User } from "../models/user.model.js";
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
 
 const registerUser = asyncHandler(async (req,res)=>{
-    res.status(200).json
-    ({
-        message:"ok pooja"
+    // res.status(200).json
+    // ({
+    //     message:"ok pooja"
+    // })
+
+    const {fullName,email,username,password} = req.body
+    console.log("email",email)
+
+    // if(fullName === ""){
+    //     throw new ApiError(400,"Fullname is required")
+    // }
+
+    if (
+        [fullName,email,username,password].some((field)=>
+        field?.trim()==="")
+    ){
+    throw new ApiError(400,"All fields are required")
+    }
+// $or --> can check multiple values in an object
+    const existedUser = User.findOne({
+        $or:[{ email },{ username }]
     })
+    if(existedUser){
+        throw new ApiError(409,"User already existed")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar file is required")
+    }
+
+    // upload images to cloudinary bt passing the localfile path
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!avatar){
+       throw new ApiError(400,"Avatar file is required")
+    }
+
+    const user = await User.create({
+        fullName,
+        avatar:avatar.url,
+        coverImage:coverImage?.url || "",
+        email,
+        password,
+        username:username.toLowerCase()
+    })
+// .select(return the selected files - can deselect using -password or -filedname)
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createdUser){
+        throw new ApiError(500,"someting went wrong while registering the user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200,createdUser,"user registered successfully..!")
+    )
 })
 
 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -54,7 +54,7 @@ const userSchema = new Schema(
 userSchema.pre("save",async function (next){
     // will encrypt the passwor donly when that field is modified
     if(!this.isModified("password")) return next();
-    this.password=bcrypt.hash(this.password,10)
+    this.password= await bcrypt.hash(this.password,10)
     next()
 })
 
@@ -87,4 +87,4 @@ userSchema.methods.generateRefreshToken = function(){
 }
 
 
-export const user = mongoose.model("User",userSchema)
+export const User = mongoose.model("User",userSchema)
